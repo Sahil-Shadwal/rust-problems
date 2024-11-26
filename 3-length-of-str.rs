4f5c1c947b95d46a249a32c63a7db52e62cf2562
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-11-26 12:39:49 +0530

    Add function to calculate the length of a string and print greeting

---
+++ 3-length-of-str.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let name = &String::from("Shadwal");
+    let len = get_str_len(name);
+    println!("Hello, {}!", name);
+    println!("The length of your name is, {}!", len);
+
+}
+
+fn get_str_len(s: &String) -> usize {
+    s.chars().count()
+}
