9d831ab5ad19b836a6365c2e8a953f8726762985
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-11-28 17:55:27 +0530

    Add JavaScript class example for rectangle area calculation

--- 5-area_of_rectangle.rs
+++ 5-area_of_rectangle.rs
@@ -2,7 +2,21 @@ struct Rect {
     width : i32,
     height : i32,
 }
+// this in a js class
+/*
+class Rect {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
 
+    area() {
+        return this.width * this.height;
+    }
+}
+    const rect = new Rect(10, 20);
+    console.log(rect.area());
+*/
 impl Rect {
     fn area(&self) -> i32 {
         self.width * self.height
