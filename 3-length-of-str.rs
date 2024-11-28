0f553c0a2847a7e68b5f0f58e15efd5d0fcd15d1
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-11-28 16:59:27 +0530

    length of string

--- 3-length-of-str.rs
+++ 3-length-of-str.rs
@@ -1,11 +1,21 @@
-fn main() {
-    let name = &String::from("Shadwal");
-    let len = get_str_len(name);
-    println!("Hello, {}!", name);
-    println!("The length of your name is, {}!", len);
+// fn main() {
+//     let name = &String::from("Shadwal");
+//     let len = get_str_len(name);
+//     println!("Hello, {}!", name);
+//     println!("The length of your name is, {}!", len);
 
+// }
+
+// fn get_str_len(s: &String) -> usize {
+//     s.chars().count()
+// }
+
+fn main(){
+    let name = String::from("Shadwal");
+    let len = get_str_len(name);
+    println!("Hello, {}!", len);
 }
 
-fn get_str_len(s: &String) -> usize {
-    s.chars().count()
+fn get_str_len(str: String) -> usize {
+    str.chars().count()
 }
